8f15a3f4c05fb4849c3fa1a3b0e900b94e82b023
Colby Pearce <crpearce892@gmail.com>
2022-11-15 10:20:52 -0700

    add id to url container for cypress testing, complete tests for url container displaying to DOM

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,25 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Shortened Urls', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
+      response: 200,
+      fixture: 'getUrl.json'
+    })
+      .as('initialIntercept')
+      .visit('http://localhost:3000/')
+  })
+
+  it('Should display the main title of the page on load', () => {
+    cy.get('h1').should('exist')
+    cy.contains('h1', 'URL Shortener')
+  })
+  it('Should display the urls contained in the server on page load', () => {
+    cy.get('#url1').should('exist')
+    cy.get('#url1').contains('Awesome photo test')
+    cy.get('#url1').contains("http://localhost:3001/useshorturl/1")
+    cy.get('#url1').contains("https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80")
+    cy.get('#url2').should('exist')
+    cy.get('#url2').contains('Hockey Url test')
+    cy.get('#url2').contains("http://localhost:3001/useshorturl/2")
+    cy.get('#url2').contains("https://www.nhl.com/standings/2022/division")
   })
 })
